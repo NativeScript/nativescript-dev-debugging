89e90168af73526769b8fdc862015df3dc9171d9
VladimirAmiorkov <vladimir.amiorkov@progress.com>
2018-12-06 18:37:11 +0200

    chore: Refactored help implementation

--- src/scripts/help.js
+++ src/scripts/help.js
@@ -1,35 +1,59 @@
 const fs = require('fs');
 const chalk = require('chalk');
+const prompter = require('cli-prompter');
 const log = console.log;
 var jsonHelper = require('./json-tags-helper');
 let jsonFile = fs.readFileSync(__dirname + "/nd-package.json");
 var jsonObject = JSON.parse(jsonFile);
-const helpCategory = process.argv[2];
-writeCommandsToConsole(jsonHelper.getDescriptionsDictionary(jsonObject));
+const inputCategoryKey = "category";
 
-function writeCommandsToConsole(scriptsDict) {
-    var categories = jsonHelper.getCategoriesDictionary(jsonObject);
-    if (!helpCategory) {
-        var tempArray = [];
-        categories.forEach(category => {
-            tempArray.push(category.value);
-        });
-        var uniqueCategories = tempArray.filter(onlyUniqueComparer);
-        log(chalk.blue("You can use params after 'npm run nd.help <param>' to sort commands by their main functionality. Available params: ") + " all, " + uniqueCategories.join(", "));
-        log(chalk.blue("Example: 'npm run nd.help run'"));
-    } else {
-        log(chalk.blue("Available Commands:\n \n"));
-        log(chalk.green("NPM command") + " : " + chalk.yellow("Description\n"));
-        for (i = 0; i < scriptsDict.length; i++) {
-            var scriptPair = scriptsDict[i];
-            var command = scriptPair.key;
-            var description = scriptPair.value;
-            if (!helpCategory) {
+var categories = jsonHelper.getCategoriesDictionary(jsonObject);
+var tempArray = ["all"];
+categories.forEach(category => {
+    tempArray.push(category.value);
+});
+var uniqueCategories = tempArray.filter(onlyUniqueComparer);
+
+const questions = [
+    {
+        type: 'autocomplete',
+        name: inputCategoryKey,
+        default: "all",
+        message: "What type of commands do you need help with ?",
+        suggest: suggestLicenses,
+    }];
+
+prompter(questions, (err, values) => {
+    if (err) {
+        writeErrorMessage(err);;
+    }
+
+    var inputParams = {
+        category: undefined
+    };
+    inputParams.category = values[inputCategoryKey];
+    writeCommandsToConsole(jsonHelper.getDescriptionsDictionary(jsonObject), inputParams.category);
+})
+
+
+function suggestLicenses({ input }, cb) {
+    const suggested = uniqueCategories;
+    cb(null, suggested)
+}
+
+
+function writeCommandsToConsole(scriptsDict, category) {
+    log(chalk.blue("Available Commands:\n \n"));
+    log(chalk.green("NPM command") + " : " + chalk.yellow("Description\n"));
+    for (i = 0; i < scriptsDict.length; i++) {
+        var scriptPair = scriptsDict[i];
+        var command = scriptPair.key;
+        var description = scriptPair.value;
+        if (!category) {
+            log(chalk.green(command) + " : " + chalk.yellow(description));
+        } else {
+            if (category == categories[i].value || category == "all") {
                 log(chalk.green(command) + " : " + chalk.yellow(description));
-            } else {
-                if (helpCategory == categories[i].value || helpCategory == "all") {
-                    log(chalk.green(command) + " : " + chalk.yellow(description));
-                }
             }
         }
     }
@@ -38,5 +62,3 @@ function writeCommandsToConsole(scriptsDict) {
 function onlyUniqueComparer(value, index, self) {
     return self.indexOf(value) === index;
 }
-
-module.exports.getHelpContent = writeCommandsToConsole;
