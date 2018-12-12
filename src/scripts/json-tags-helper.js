bf4c4d343a713f747fbe363c3c9c54630d10ffec
VladimirAmiorkov <vladimir.amiorkov@progress.com>
2018-12-12 11:58:06 +0200

    chore: Code cleaning

--- src/scripts/json-tags-helper.js
+++ src/scripts/json-tags-helper.js
@@ -1,12 +1,3 @@
-function getScriptsDictionary(json) {
-    return getDictionary(json["scripts"]);
-
-}
-
-function getDevDepsDictionary(json) {
-    return getDictionary(json["devDependencies"]);
-}
-
 function getDescriptionsDictionary(json) {
     return getDictionary(json["descriptions"]);
 }
@@ -24,7 +15,5 @@ function getDictionary(json) {
     return result;
 }
 
-module.exports.getScriptsDictionary = getScriptsDictionary;
-module.exports.getDevDepsDictionary = getDevDepsDictionary;
 module.exports.getDescriptionsDictionary = getDescriptionsDictionary;
 module.exports.getCategoriesDictionary = getCategoriesDictionary;
