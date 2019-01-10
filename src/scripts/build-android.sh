52ef607b82fb172d15f4816015e74f916548dc98
VladimirAmiorkov <vladimir.amiorkov@progress.com>
2019-01-10 11:10:05 +0200

    chore: Code cleaning

--- src/scripts/build-android.sh
+++ src/scripts/build-android.sh
@@ -16,15 +16,12 @@ done
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# ANDROID_SOURCE_DIR="$CURRENT_DIR/../../../../src-native/android"
-
 PROJECT_NAME="$SOURCE_NAME"
 RELEASE_AAR_FILE=$PROJECT_NAME-release.aar
 DEBUG_AAR_FILE=$PROJECT_NAME-Debug.aar
 
 BUILD_OUTPUT_DIR="$ANDROID_SOURCE_DIR/$PROJECT_NAME/build/outputs/aar/"
 
-# PLUGIN_TARGET_DIR="$CURRENT_DIR/../../../../src/platforms"
 PLUGIN_TARGET_SUBDIR="$PLUGIN_TARGET_DIR/android"
 
 OUTPUT_RELEASE_AAR_FILE_DIR=$PLUGIN_TARGET_SUBDIR/$RELEASE_AAR_FILE
@@ -62,5 +59,3 @@ cp -R "$BUILD_OUTPUT_DIR/$AAR_FILE" $PLUGIN_TARGET_SUBDIR
 echo "force livesync" > "$PLUGIN_TARGET_SUBDIR/sync"
 
 echo "Android library was copied to $PLUGIN_TARGET_SUBDIR"
-
-# cd $CURRENT_DIR
